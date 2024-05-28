e840ac6ec91aa3079e8c5943706c541262135882
gimgyumin <qmean59@gmail.com>
2024-05-29 00:04:53 +0900

    Object class의 clone 메서드 학습

---
+++ object_class/src/main/java/study/clone/Clone.java
@@ -0,0 +1,50 @@
+package study.clone;
+
+public class Clone {
+    public static void main(String[] args) {
+        TestA original = new TestA(3, new TestB(5));
+        TestA clone = original.clone();
+        System.out.println("original = " + original);
+        System.out.println("original.data = " + original.data);
+        System.out.println("original.classData = " + original.classData);
+        System.out.println("original.classData.data = " + original.classData.data);
+        System.out.println("==================================================");
+        System.out.println("clone = " + clone);
+        System.out.println("clone.data = " + clone.data);
+        System.out.println("clone.classData = " + clone.classData);
+        System.out.println("clone.classData.data = " + clone.classData.data);
+        System.out.println("==================================================");
+        System.out.println("original.classData의 Data 변경");
+        System.out.println("==================================================");
+        original.classData.data = 10;
+        System.out.println("original.classData.data = " + original.classData.data);
+        System.out.println("clone.classData.data = " + clone.classData.data);
+    }
+}
+
+class TestA implements Cloneable {
+    int data;
+    TestB classData;
+
+    public TestA(int data, TestB classData) {
+        this.data = data;
+        this.classData = classData;
+    }
+
+    @Override
+    public TestA clone() {
+        try {
+            return (TestA) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+}
+
+class TestB {
+    int data;
+
+    public TestB(int data) {
+        this.data = data;
+    }
+}
---
+++ object_class/src/main/java/study/clone/TwoWaysOfCopy.java
@@ -0,0 +1,95 @@
+package study.clone;
+
+public class TwoWaysOfCopy {
+    public static void main(String[] args) {
+        Gun original = new Gun(10, new Bullet(3));
+        Gun clone = original.clone();
+        Gun swallowCopy = original.swallowCopy();
+        Gun deepCopy = original.deepCopy();
+
+        System.out.println("original = " + original);
+        System.out.println("clone = " + clone);
+        System.out.println("swallowCopy = " + swallowCopy);
+        System.out.println("deepCopy = " + deepCopy);
+        System.out.println("==========================");
+        original.bullet.size = 100;
+        System.out.println("original.bullet.size changed");
+        System.out.println("==========================");
+        System.out.println("original = " + original);
+        System.out.println("clone = " + clone);
+        System.out.println("swallowCopy = " + swallowCopy);
+        System.out.println("deepCopy = " + deepCopy);
+    }
+}
+
+class Gun implements Cloneable {
+    int size;
+    Bullet bullet;
+
+    @Override
+    public Gun clone() {
+        try {
+            return (Gun) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+
+    public Gun deepCopy() {
+        Object obj = null;
+        try {
+            obj = super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+        Gun g = (Gun) obj;
+        g.bullet = this.bullet.clone();
+        return (Gun) obj;
+    }
+
+    public Gun swallowCopy() {
+        Object obj = null;
+        try {
+            return (Gun) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Gun(int size, Bullet bullet) {
+        this.size = size;
+        this.bullet = bullet;
+    }
+
+    @Override
+    public String toString() {
+        return "Gun{" +
+                "size=" + size +
+                ", bullet=" + bullet +
+                '}';
+    }
+}
+
+class Bullet implements Cloneable{
+    int size;
+
+    public Bullet(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public Bullet clone() {
+        try {
+            return (Bullet) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Bullet{" +
+                "size=" + size +
+                '}';
+    }
+}
